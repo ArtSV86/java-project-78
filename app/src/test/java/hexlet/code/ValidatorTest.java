a9bf28faaa4bb7d90e2121eb13c04e28bb300400
Artur Sadov <Art.S.V.86@yandex.ru>
2023-12-06 17:56:28 +0400

    Fix

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class ValidatorTest {
 
